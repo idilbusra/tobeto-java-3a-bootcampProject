2b5bce837fa8ab79b1d828a525a6a5b6569cd7b9
busra idil <diyetisyenbusraidil@gmail.com>
2024-02-21 03:22:47 +0300

    Layers created

---
+++ bootcampProject/src/main/java/com/tobeto/bootcampProject/entities/Instructor.java
@@ -0,0 +1,17 @@
+package com.tobeto.bootcampProject.entities;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Instructor extends User {
+
+    private String companyName;
+}
